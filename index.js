757239e3f7fd91b0d14f3546c189c90f8e80543e
Vladimir Kudinov <frux@yandex-team.ru>
2015-09-30 13:26:35 +0500

    Fix callback problem

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var Uppercode = {
-        exec: exec,
-        execSync: execSync,
-        stagedFiles: stagedFiles,
-        stagedFilesSync: stagedFilesSync,
-        globalModules: globalModules,
-        globalModulesSync: globalModulesSync,
-        trigger: triggerHook
+	exec: exec,
+	execSync: execSync,
+	stagedFiles: stagedFiles,
+	stagedFilesSync: stagedFilesSync,
+	globalModules: globalModules,
+	globalModulesSync: globalModulesSync,
+	trigger: triggerHook
     },
     ChildProcess = require('child_process');
 
@@ -16,27 +16,28 @@ var Uppercode = {
  * @returns {string}
  */
 function execSync(command, args){
-    var commandProcess,
-        result;
+	var commandProcess,
+	result;
 
-    if(typeof command === 'string'){
+	if(typeof command === 'string'){
 
-        //if arguments is not an array replace it by empty array
-        if(args instanceof Array){
-            args = args.join(' ');
-        }else{
-            args = '';
-        }
+		//if arguments is not an array replace it by empty array
+		if(args instanceof Array){
+			args = args.join(' ');
+		}else{
+			args = '';
+		}
 
-        try{
-            //run command
-            result = ChildProcess.execSync(command + ' ' + args).toString().replace(/\n$/, '');
-        }catch(e){
-            return;
-        }
+		try{
 
-        return result;
-    }
+			//run command
+			result = ChildProcess.execSync(command + ' ' + args).toString().replace(/\n$/, '');
+		}catch(e){
+			return;
+		}
+
+		return result;
+	}
 }
 
 /**
@@ -46,16 +47,24 @@ function execSync(command, args){
  * @param callback {Function|undefined} Callback function
  */
 function exec(command, args, callback){
-    var data,
-        err;
+	var data,
+	err;
 
-    try{
-        data = execSync(command, args);
-    }catch(e){
-        err = e;
-    }
+	if(typeof callback !== 'function'){
+		if(typeof args === 'function'){
+			callback = args;
+		}else{
+			callback = function(){};
+		}
+	}
 
-    callback(err, data);
+	try{
+		data = execSync(command, args);
+	}catch(e){
+		err = e;
+	}
+
+	callback(err, data);
 }
 
 /**
@@ -64,31 +73,31 @@ function exec(command, args, callback){
  * @returns {Array}
  */
 function stagedFilesSync(changeFilter){
-    var commandResult,
-        files;
+	var commandResult,
+	files;
 
-    //if the first argument is provided
-    if(typeof changeFilter === 'string'){
+	//if the first argument is provided
+	if(typeof changeFilter === 'string'){
 
-        //if filter is not valid exit
-        if(!/^[ACDMRTUXB\*]+$/.test(changeFilter)){
-            return;
-        }
+		//if filter is not valid exit
+		if(!/^[ACDMRTUXB\*]+$/.test(changeFilter)){
+			return;
+		}
 
-        //if filter was not specified use default
-    }else{
-        changeFilter = '*';
-    }
+		//if filter was not specified use default
+	}else{
+		changeFilter = '*';
+	}
 
-    try{
-        commandResult = execSync('git', ['diff', '--cached', '--name-only', '--diff-filter=' + changeFilter]);
-    }catch(e){
-        throw e;
-    }
+	try{
+		commandResult = execSync('git', ['diff', '--cached', '--name-only', '--diff-filter=' + changeFilter]);
+	}catch(e){
+		throw e;
+	}
 
-    files = commandResult.split('\n');
+	files = commandResult.split('\n');
 
-    return files;
+	return files;
 }
 
 /**
@@ -97,16 +106,22 @@ function stagedFilesSync(changeFilter){
  * @param callback {Function} Callback function
  */
 function stagedFiles(changeFilter, callback){
-    var data,
-        err;
+	var data,
+	err;
 
-    try{
-        data = stagedFilesSync(changeFilter);
-    }catch(e){
-        err = e;
-    }
+	if(typeof changeFilter === 'function'){
+		callback = changeFilter;
+	}else{
+		callback = function(){};
+	}
 
-    callback(err, data);
+	try{
+		data = stagedFilesSync(changeFilter);
+	}catch(e){
+		err = e;
+	}
+
+	callback(err, data);
 }
 
 /**
@@ -116,34 +131,34 @@ function stagedFiles(changeFilter, callback){
  * @returns {Array}
  */
 function globalModulesSync(grep, nameOnly){
-    var commandResult,
-        modules;
+	var commandResult,
+	modules;
 
-    if(typeof grep === 'boolean'){
-        nameOnly = grep;
-    }
+	if(typeof grep === 'boolean'){
+		nameOnly = grep;
+	}
 
-    if(typeof grep === 'string'){
-        grep = ' | grep ' + grep;
-    }else{
-        grep = '';
-    }
+	if(typeof grep === 'string'){
+		grep = ' | grep ' + grep;
+	}else{
+		grep = '';
+	}
 
-    commandResult = execSync('npm', ['ls', '-g', '--depth=0', '--parseable' + grep]);
+	commandResult = execSync('npm', ['ls', '-g', '--depth=0', '--parseable' + grep]);
 
-    if(commandResult){
-        modules = commandResult.split('\n');
+	if(commandResult){
+		modules = commandResult.split('\n');
 
-        if(nameOnly){
-            modules = modules.map(function(modulePath){
-                return modulePath.substr(modulePath.lastIndexOf('/') + 1);
-            });
-        }
-    }else{
-        modules = [];
-    }
+		if(nameOnly){
+			modules = modules.map(function(modulePath){
+				return modulePath.substr(modulePath.lastIndexOf('/') + 1);
+			});
+		}
+	}else{
+		modules = [];
+	}
 
-    return modules;
+	return modules;
 }
 
 /**
@@ -154,16 +169,26 @@ function globalModulesSync(grep, nameOnly){
  */
 function globalModules(grep, nameOnly, callback){
 
-    var data,
-        err;
+	var data,
+	err;
 
-    try{
-        data = globalModulesSync(grep, nameOnly);
-    }catch(e){
-        err = e;
-    }
+	if(typeof callback !== 'function'){
+		if(typeof nameOnly === 'function'){
+			callback = nameOnly;
+		}else if(typeof grep === 'function'){
+			callback = grep;
+		}else{
+			callback = function(){};
+		}
+	}
 
-    callback(err, data);
+	try{
+		data = globalModulesSync(grep, nameOnly);
+	}catch(e){
+		err = e;
+	}
+
+	callback(err, data);
 }
 
 /**
@@ -172,76 +197,76 @@ function globalModules(grep, nameOnly, callback){
  * @param callback {Function} Callback function
  */
 function triggerHook(event, callback){
-    var plugins,
-        timestamp;
+	var plugins,
+	timestamp;
 
-    //event should be a string
-    if(typeof event === 'string'){
+	//event should be a string
+	if(typeof event === 'string'){
 
-        //if callback is not a function use empty function
-        if(typeof callback !== 'function'){
-            callback = function(){};
-        }
+		//if callback is not a function use empty function
+		if(typeof callback !== 'function'){
+			callback = function(){};
+		}
 
-        //get list of locally installed plugins started with "uppercode-" split it by rows, trim path, require it
-        plugins = execSync('ls -1 ' + __dirname + '/.. | grep uppercode-').split('\n').map(function(plugin){
-            return require(plugin.substr(plugin.lastIndexOf('/') + 1))[event];
-        }).filter(function(plugin){ return !!plugin });
+		//get list of locally installed plugins started with "uppercode-" split it by rows, trim path, require it
+		plugins = execSync('ls -1 ' + __dirname + '/.. | grep uppercode-').split('\n').map(function(plugin){
+			return require(plugin.substr(plugin.lastIndexOf('/') + 1))[event];
+		}).filter(function(plugin){ return !!plugin; });
 
-        /**
-         * Calls next hook
-         */
-        function next(){
-            var plugin;
+		/**
+		         * Calls next hook
+		         */
+		function next(){
+			var plugin;
 
-            //if there are plugins which haven't worked yet
-            if(plugins.length){
+			//if there are plugins which haven't worked yet
+			if(plugins.length){
 
-                //get name of the next plugin in a list
-                plugin = plugins.shift();
+				//get name of the next plugin in a list
+				plugin = plugins.shift();
 
-                //if this plugin exists
-                if(plugin){
+				//if this plugin exists
+				if(plugin){
 
-                    //call it
-                    plugin.call(Uppercode, next);
+					//call it
+					plugin.call(Uppercode, next);
 
-                }else{
+				}else{
 
-                    //call next
-                    next();
-                }
-            }else{
+					//call next
+					next();
+				}
+			}else{
 
-                //finish hook
-                finish();
-            }
-        }
+				//finish hook
+				finish();
+			}
+		}
 
-        /**
-         * Start hook with calling the first plugin
-         */
-        function start(){
-            timestamp = +new Date;
-            console.log('==================\nUppercode started:');
-            next();
-        }
+		/**
+		         * Start hook with calling the first plugin
+		         */
+		function start(){
+			timestamp = +new Date;
+			console.log('==================\nUppercode started:');
+			next();
+		}
 
-        /**
-         * Finish hook
-         */
-        function finish(){
-            callback();
-            console.log('Uppercode has finished in ' + (+new Date - timestamp) + 'ms\n==================');
-        }
+		/**
+		         * Finish hook
+		         */
+		function finish(){
+			callback();
+			console.log('Uppercode has finished in ' + (+new Date - timestamp) + 'ms\n==================');
+		}
 
-        //if there is any plugin
-        if(plugins.length){
+		//if there is any plugin
+		if(plugins.length){
 
-            //start hook
-            start();
-        }
-    }
+			//start hook
+			start();
+		}
+	}
 }
 
 module.exports = Uppercode;
